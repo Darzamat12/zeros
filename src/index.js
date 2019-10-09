0be3810db6bb6d3ccee026b9d5ca1b7018ded405
Darzamat12 <heavylesha@gmail.com>
2019-10-10 00:51:39 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let Array=expression.split('*');
+  let arrayOfNumbers=[];
+  let tmp;
+  let result=0;
+  let counter5=0;
+  let counterOdd=0;
+  let anotherArray=[];
+
+  Array.forEach(function(element) {
+    if(element.includes('!!')){
+      tmp=element.replace(/!!/g, "");
+      for(let i=tmp;i>0;i-=2){
+        arrayOfNumbers.push(i);
+      }
+    
+    }
+
+    else {
+      tmp=element.replace(/!/g,"");
+      for(let i=tmp;i>0;i--){
+        arrayOfNumbers.push(i);
+      }
+    }
+  });
+
+  arrayOfNumbers.forEach(function(element){
+    if(element%100==0){ result+=2; anotherArray.push(element/100);}
+    else if(element%10==0) {result++; anotherArray.push(element/10);}
+    else if(element%5==0) {counter5++; anotherArray.push(element/5);}
+    else if(element%2==0) {counterOdd++; anotherArray.push(element/2);}
+
+
+  });
+  anotherArray.forEach(function(element){
+    if(element%100==0){ result+=2;}
+    else if(element%10==0) {result++;}
+    else if(element%5==0) {counter5++;}
+    else if(element%2==0) {counterOdd++;}
+
+
+  });
+
+  result+=Math.min(counter5,counterOdd);
+
+  return result;
 }
+
+
+//console.log(zeros('10!!*20!!*30!!'));
